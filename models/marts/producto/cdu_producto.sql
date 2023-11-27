f0358319afa5808f90399f9e15ae8a1ebb2fdc8b
Alejandro Arco <alexarcosarria@gmail.com>
2023-11-27 15:15:36 +0000

    cdu_products

--- models/marts/producto/cdu_producto.sql
+++ models/marts/producto/cdu_producto.sql
@@ -18,12 +18,11 @@ renamed as (
         u.email,
         min(created_at) as start_sesion,
         max(created_at) as end_sesion,
-        datediff(minute, min(created_at), max(created_at))::number(10,0) as sesion_duration_minutes,
-        count(page_url) as pages_views,
+        datediff(minute, min(created_at::timestamp_ntz(9)), max(created_at::timestamp_ntz(9)))::number(10,0) as sesion_duration_minutes,
+        sum(case when e.event_type = 'page_view' then 1 end) as page_view_amount,
         sum(case when e.event_type = 'checkout' then 1 end) as checkout_amount,
         sum(case when e.event_type = 'package_shipped' then 1 end) as package_shipped_amount,
-        sum(case when e.event_type = 'add_to_cart' then 1 end) as add_to_cart_amount,
-        sum(case when e.event_type = 'page_view' then 1 end) as page_view_amount
+        sum(case when e.event_type = 'add_to_cart' then 1 end) as add_to_cart_amount
         
     from events as e
     inner join
